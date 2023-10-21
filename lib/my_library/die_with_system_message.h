96198275766e3599f641568f35a51be89791f27e
Today <ipp.15122098@gmail.com>
2023-10-22 04:28:59 +0900

    [clean] Refactoring

--- lib/my_library/die_with_system_message.h
+++ lib/my_library/die_with_system_message.h
@@ -1,5 +1,5 @@
-#ifndef MY_LIBRARY_DIE_WITH_SYSTEM_MESSAGE_H
-#define MY_LIBRARY_DIE_WITH_SYSTEM_MESSAGE_H 1
+#ifndef C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_DIE_WITH_SYSTEM_MESSAGE_H
+#define C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_DIE_WITH_SYSTEM_MESSAGE_H 1
 
 // Print system message and terminate program.
 void DieWithSystemMessage(int line, const char *msg);
--- lib/my_library/directory.h
+++ lib/my_library/directory.h
@@ -1,5 +1,5 @@
-#ifndef MY_LIBRARY_DIRECTORY_H
-#define MY_LIBRARY_DIRECTORY_H 1
+#ifndef C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_DIRECTORY_H
+#define C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_DIRECTORY_H 1
 
 const char *DATA_DIR_PATH = "data/";      // Path of directory including sample data.
 const char *OUTPUT_DIR_PATH = "output/";  // Path of directory for output.
