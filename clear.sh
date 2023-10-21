2f78a5d940bd8815a67220c638def8d3123aa5c5
Today <ipp.15122098@gmail.com>
2023-10-22 02:24:45 +0900

    [add] Create a shell script to clear workspace

---
+++ clear.sh
@@ -0,0 +1,11 @@
+# Remove output files.
+echo '> rm -r output'
+rm -rf output
+
+# Remove exec files.
+echo '> rm *.out'
+rm *.out
+
+# Remove "build/" directory.
+echo '> rm -r build'
+rm -rf build
