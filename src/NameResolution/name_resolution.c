f1d6ed307d6972359d26719b7a0c90f802146c23
Today <ipp.15122098@gmail.com>
2023-10-24 02:35:59 +0900

    [clean] Refactoring

--- src/NameResolution/name_resolution.c
+++ src/NameResolution/name_resolution.c
@@ -6,7 +6,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-void Usage(char *const argv[]) {
+void Usage(char *argv[]) {
     fprintf(stderr,
             "Usage: %s <hostname> [port number|service]\n"
             "Name Resolution\n",
@@ -38,29 +38,35 @@ int main(int argc, char *argv[]) {
     int cnt = 0;
     struct addrinfo *result;
     for(result = result0; result; result = result->ai_next) {
+        // アドレスファミリ．
         printf("[%d] address family: ", ++cnt);
         if(result->ai_family == AF_INET) printf("INET (IPv4)");
         else if(result->ai_family == AF_INET6) printf("INET6 (IPv6)");
-        else printf("other");
+        else printf("other (%d)", result->ai_family);
         printf("\n");
 
+        // ソケットタイプ．
         printf("    socket type:    ");
         if(result->ai_socktype == SOCK_STREAM) printf("stream socket (TCP)");
         else if(result->ai_socktype == SOCK_DGRAM) printf("datagram socket (UDP)");
-        else printf("%d", result->ai_socktype);
+        else if(result->ai_socktype == SOCK_RAW) printf("raw socket");
+        else printf("other (%d)", result->ai_socktype);
         printf("\n");
 
+        // ソケットアドレス．
         if(result->ai_family == AF_INET) {
             char buf[INET_ADDRSTRLEN] = {};
             struct in_addr addr;
             addr.s_addr = ((struct sockaddr_in *)(result->ai_addr))->sin_addr.s_addr;
             inet_ntop(AF_INET, &addr, buf, INET_ADDRSTRLEN);
-            printf("    ip address:     %s:%d\n", buf, ntohs(((struct sockaddr_in *)(result->ai_addr))->sin_port));
+            uint16_t port = ntohs(((struct sockaddr_in *)(result->ai_addr))->sin_port);
+            printf("    ip address:     %s:%d\n", buf, port);
         } else if(result->ai_family == AF_INET6) {
             char buf[INET6_ADDRSTRLEN] = {};
             unsigned char *const p = ((struct sockaddr_in6 *)(result->ai_addr))->sin6_addr.s6_addr;
             inet_ntop(AF_INET6, p, buf, INET6_ADDRSTRLEN);
-            printf("    ip address:     %s.%d\n", buf, ntohs(((struct sockaddr_in6 *)(result->ai_addr))->sin6_port));
+            uint16_t port = ntohs(((struct sockaddr_in6 *)(result->ai_addr))->sin6_port);
+            printf("    ip address:     %s.%d\n", buf, port);
         }
     }
 
