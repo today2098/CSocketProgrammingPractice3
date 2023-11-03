1746c9dec92e359af6564f55c706d96186352e0e
Today <ipp.15122098@gmail.com>
2023-11-03 16:50:15 +0900

    [update] Create new function, "IsSocketAddressEqual()"

--- lib/my_library/handle_socket_address.c
+++ lib/my_library/handle_socket_address.c
@@ -1,4 +1,4 @@
-#include "get_socket_address.h"
+#include "handle_socket_address.h"
 
 #include <arpa/inet.h>
 #include <stdint.h>
@@ -30,6 +30,22 @@ int GetSocketAddress(const struct sockaddr *saddr, char buf[], size_t len) {
         snprintf(buf, len, "[%s]:%d", buf0, port);
     } else {
         snprintf(buf, len, "[unknown type]");
+    }
+    return 0;
+}
+
+int IsSocketAddressEqual(const struct sockaddr *saddr1, const struct sockaddr *saddr2) {
+    if(saddr1 == NULL || saddr2 == NULL) return saddr1 == saddr2;
+    if(saddr1->sa_family == saddr2->sa_family) {
+        if(saddr1->sa_family == AF_INET) {
+            const struct sockaddr_in *p1 = (struct sockaddr_in *)saddr1;
+            const struct sockaddr_in *p2 = (struct sockaddr_in *)saddr2;
+            return p1->sin_addr.s_addr == p2->sin_addr.s_addr && p1->sin_port == p2->sin_port;
+        } else if(saddr1->sa_family == AF_INET6) {
+            const struct sockaddr_in6 *p1 = (struct sockaddr_in6 *)saddr1;
+            const struct sockaddr_in6 *p2 = (struct sockaddr_in6 *)saddr2;
+            return memcmp(&p1->sin6_addr, &p2->sin6_addr, sizeof(struct in6_addr)) == 0 && p1->sin6_port == p2->sin6_port;
+        }
     }
     return 0;
 }
--- lib/my_library/handle_socket_address.h
+++ lib/my_library/handle_socket_address.h
@@ -1,5 +1,5 @@
-#ifndef C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_GET_SOCKET_ADDRESS_H
-#define C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_GET_SOCKET_ADDRESS_H 1
+#ifndef C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_HANDLE_SOCKET_ADDRESS_H
+#define C_SOCKET_PROGRAMMING_PRACTICE_MY_LIBRARY_HANDLE_SOCKET_ADDRESS_H 1
 
 #include <stddef.h>
 #include <sys/socket.h>
@@ -16,5 +16,14 @@
  * @return 0 is returned if there was no error. On error, -1 is returned.
  */
 int GetSocketAddress(const struct sockaddr *saddr, char buf[], size_t len);
+
+/**
+ * @brief Determine whether two socket addresses are equal.
+ *
+ * @param saddr1 socket address 1
+ * @param saddr2 socket address 2
+ * @return 1 is returned if two addresses are equal. If not equal, 0 is returned.
+ */
+int IsSocketAddressEqual(const struct sockaddr *saddr1, const struct sockaddr *saddr2);
 
 #endif
